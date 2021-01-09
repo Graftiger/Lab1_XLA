0c0bbea3fa8153eb2f79e88e7817afe6690d8243
Graftiger <71170323+Graftiger@users.noreply.github.com>
2021-01-09 17:31:02 +0500

    Add files via upload

---
+++ XToDoList/XToDoList/ToDoItemCell.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XToDoList
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ToDoItemCell : ViewCell
+    {
+        public ToDoItemCell()
+        {
+            InitializeComponent();
+        }
+
+        private void OnEditBtnClick(object sender, EventArgs e)
+        {
+            var mainPage = Application.Current.MainPage;
+            mainPage.Navigation.PushModalAsync(new EditPage());
+        }
+    }
+}
